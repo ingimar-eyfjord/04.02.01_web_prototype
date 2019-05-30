0e0e07a8e4f12a44a6a3677c4544186f1764d313
Ingimar <ingimareys93@gmail.com>
2019-05-30 23:52:53 +0200

    Version before testing

--- Javascript.js
+++ Javascript.js
@@ -11,7 +11,7 @@ window.onclick = function(event) {
 }
 
 function FirstsemesterOpen()
-	{document.getElementById("first-semester-closed").setAttribute("class", "first-semester-open");	
+	{document.getElementById("first-semester-closed").classList.toggle("first-semester-open");	
 	document.getElementById("module-1").setAttribute("class", "moduleoneopen");document.getElementById("module-2").setAttribute("class", "moduletwoopen");document.getElementById("module-3").setAttribute("class", "modulethreeopen");document.getElementById("module-4").setAttribute("class", "modulefouropen");}
 
 function firstintro(){
@@ -35,8 +35,8 @@ function secondintro(){
     z.style.display = "block";
   } else {
     z.style.display = "none";
-  }
-document.getElementById("intro-text-three").setAttribute("class", "intro-text-1");var p = document.getElementById("intro-button-2");
+  }document.getElementById("intro-text-three").setAttribute("class", "intro-text-1");
+	var p = document.getElementById("intro-button-2");
   if (p.style.display === "none") {
     p.style.display = "block";
   } else {
@@ -54,3 +54,31 @@ function openleftbox(){document.getElementById("leftbox-closed").setAttribute("c
 
 function leftboxclose(){document.getElementById("leftbox-closed").setAttribute("class","");
 						document.getElementById("leftbox-closed").setAttribute("class","leftbox-closed");}
+function Teachingcurrenttaskdropdown(){document.getElementById("Teaching-material-closed").classList.toggle("current-task-dropdown");document.getElementById("inside-current-teach").classList.toggle("inside-currentteach")}
+
+function backwardsweek22(){document.getElementById("week22").classList.toggle("week-22");document.getElementById("week23").setAttribute("class", "week-23")}
+
+function onwardsweek23(){document.getElementById("week22").classList.toggle("week-22");document.getElementById("week23").setAttribute("class", "")}
+
+function onwardsweek24(){document.getElementById("week23").classList.toggle("week-23");document.getElementById("week24").setAttribute("class", "")}
+
+function backwardsweek23(){document.getElementById("week23").classList.toggle("week-23");document.getElementById("week24").setAttribute("class", "week-24")}
+
+function Requirementscurrenttaskdropdown(){document.getElementById("Requirements-closed").classList.toggle("current-task-dropdown");document.getElementById("inside-current").classList.toggle("inside-current")}
+
+function handcurrenttaskdropdown(){document.getElementById("hand-in-closed").classList.toggle("current-task-dropdownhand");document.getElementById("inside-currenthand").classList.toggle("inside-currenthand");}
+
+function pdfonclick()
+{document.getElementById("pdfonclick").setAttribute("class", "pdfget");}
+
+document.querySelector("login").addEventListener("click", function(event){document.getElementById("login");event.preventDefault();window.location.href = "04.02.01_web_prototype.html";
+}, false);
+
+/*
+const toggle=document.querySelectorAll(".toggle-class")
+
+toggle.forEach((n)=>{n.addEventListener("click", toggleclass)})
+
+function toggleclass(){
+	this.classList.toggle("current-task-dropdown")
+}*/
